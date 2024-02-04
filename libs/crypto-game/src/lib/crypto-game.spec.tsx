ed16f89318527a2bff03566c24c92267fa3c48cc
Pavel Kogan <pashaweb@gmail.com>
2024-02-04 14:45:02 +0200

    Test update

--- libs/crypto-game/src/lib/crypto-game.spec.tsx
+++ libs/crypto-game/src/lib/crypto-game.spec.tsx
@@ -1,37 +1,58 @@
+import { render } from '@testing-library/react'
 import { describe, expect, test } from 'vitest'
-import { render } from '@testing-library/react';
 
-import { CryptoGame } from './crypto-game';
-
+import { CryptoGame } from './crypto-game'
+import { CryptoRateLatest, CryptoRates, Wallet } from './types'
 
 describe('CryptoGame', () => {
+    beforeEach(() => {
+        localStorage.clear()
+    })
 
-  beforeEach(() => {
-    localStorage.clear();
-  });
+    test('renders without error', () => {
+        const { container } = renderComponent()
+        expect(container).toBeTruthy()
+    })
 
+    test('should initialize state from localStorage', () => {
+        const mockCurrencyRates: CryptoRates = {
+            bitcoin: [50000, 60000, 70000, 80000, 90000],
+            ethereum: [3000, 4000, 5000, 6000, 7000],
+            dogecoin: [0.1, 0.2, 0.3, 0.4, 0.5],
+            cardano: [1, 2, 3, 4, 5],
+        }
+        const mockCriptoRateLatest: CryptoRateLatest = {
+            bitcoin: 60000,
+            ethereum: 4000,
+            dogecoin: 0.2,
+            cardano: 2,
+        }
 
-  test('renders without error', () => {
-    const { container } = renderComponent();
-    expect(container).toBeTruthy();
-  });
+        const mockWallet: Wallet = {
+            dollar: 1000,
+            fee: 0.01,
+            crypto: {
+                bitcoin: 1,
+                ethereum: 2,
+                dogecoin: 3,
+                cardano: 4,
+            },
+        }
 
-  test('should initialize state from localStorage', () => {
-    const mockCurrencyRates = { USD: 1.2 };
-    const mockCriptoRateLatest = { BTC: 50000 };
-    const mockWallet = { BTC: 2 };
+        localStorage.setItem('currencyRates', JSON.stringify(mockCurrencyRates))
+        localStorage.setItem(
+            'criptoRateLatest',
+            JSON.stringify(mockCriptoRateLatest)
+        )
+        localStorage.setItem('wallet', JSON.stringify(mockWallet))
 
-    localStorage.setItem('currencyRates', JSON.stringify(mockCurrencyRates));
-    localStorage.setItem('criptoRateLatest', JSON.stringify(mockCriptoRateLatest));
-    localStorage.setItem('wallet', JSON.stringify(mockWallet));
+        const { container } = renderComponent()
+        console.log(container.innerHTML)
 
-    const { container } = renderComponent();
-    console.log(container.innerHTML);
-
-    expect(container).toBeTruthy();
-  });
-});
+        expect(container).toBeTruthy()
+    })
+})
 
 const renderComponent = () => {
-  return render(<CryptoGame />);
+    return render(<CryptoGame />)
 }
